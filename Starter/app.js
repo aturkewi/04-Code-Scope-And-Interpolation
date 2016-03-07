28059f41a09c920b882f3f7858e18a85db05d589
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-06 22:23:48 -0500

    Play with watchers

--- Starter/app.js
+++ Starter/app.js
@@ -1,7 +1,26 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', function($scope) {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
 
   $scope.handle='';
+
+  $scope.lowerCaseHandle = function() {
+    return $filter('lowercase')($scope.handle);
+  }
+
+  // This helps show the digest cycle! We can manually add something to the watch list
+  // $scope.$watch('handle', function(newValue, oldValue){
+  //
+  //   console.info('Changed!');
+  //   console.log('Old: ' + oldValue);
+  //   console.log('New: ' + newValue);
+  //
+  // })
+
+  setTimeout(function(){
+    $scope.$apply(function(){
+      $scope.handle = 'newTwitterHandle';
+    });
+  }, 3000);
 
 }]);
